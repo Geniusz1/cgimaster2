e09375ddf368c9054eefdb7a81e00db61726f3cf
Geniusz1 <genini98@gmail.com>
2021-04-03 09:08:47 +0200

    Use interface API instead
    I've decided that I'm too lazy to write my own UI lib.

--- main.lua
+++ main.lua
@@ -1,16 +1,7 @@
 require('scripts.CGImaster2.pngLua.png')
-require('scripts.CGImaster2.ui')
 
 local img = pngImage('./scripts/CGImaster2/mario.png', newRowCallback)
 local lo = img:getPixel(1,1)
 
 local w = 100
 local h = 100
-mainbox = ui.box(gfx.WIDTH/2 - w/2, gfx.HEIGHT/2 - h/2, w, h)
-mainbox.draw_background = true
-
-local function handle_tick()
-    mainbox:draw()
-end
-
-evt.register(evt.tick, handle_tick)
